ed0680b688f6901972ed24ff1157240e07f9f338
kritical0613 <kulishan0613@gmail.com>
2022-04-23 17:22:37 -0500

    fix: Wallet Modal theme

--- src/pages/PreSale/WalletModal.tsx
+++ src/pages/PreSale/WalletModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import Web3 from "web3";
 import METAMASK_ICON from "../../assets/svg/metamask.svg"
-import COINBASE_ICON from "../../assets/svg/coinbase.svg"
+import TRUSTWALLET_ICON from "../../assets/svg/trustwallet.svg"
 
 interface Props {
   isOpen: any,
@@ -30,13 +30,14 @@ const WalletModal: React.FC<Props> = ({ isOpen, connectHandler, closeHandler }) 
   return <>
     {isOpen &&
       <div className="flex w-[100%] h-[100%] m-auto bg-white/50 z-[1000] backdrop-blur-sm" onClick={closeHandler} style={{ position: "fixed", top: "0", left: "0", right: "0" }}>
-        <div className="grid min-w-[280px] md:min-w-[420px] h-[240px] m-auto px-4 bg-white rounded-lg items-center justify-center">
+        <div className="grid min-w-[280px] md:min-w-[420px] min-h-[240px] gap-4 m-auto px-4 py-4 bg-white rounded-lg items-center justify-center">
           <div className="text-[28px] text-black text-center">Connect Wallet</div>
           <div className="grid gap-4 md:flex">
             {ethEnabled() ?
-              <div className="items-center justify-center text-[24px] text-black/80 gap-2 md:gap-16 border-2 p-4 px-8 cursor-pointer grid md:flex" onClick={connectHandler}><div className="flex items-center gap-4 text-center"><img src={METAMASK_ICON} alt="metamask" width={48} /> | <img src={COINBASE_ICON} alt="coinbase" width={96} /></div><div className="w-full text-center">Injected</div></div> :
-              <a href="https://metamask.io" className="" target="_blank" rel="noreferrer"><div className="flex items-center text-[24px] text-black/80 gap-4 border-2 p-4 px-8 cursor-pointer"><img src={METAMASK_ICON} alt="metamask" width={48} /><img src={COINBASE_ICON} alt="coinbase" width={96} />Install Metamask</div></a>
+              <div className="items-center justify-center text-[24px] text-black/80 gap-2 md:gap-16 border-2 p-4 px-8 cursor-pointer grid md:flex" onClick={connectHandler}><div className="grid items-center gap-4 text-center justify-items-center"><img src={METAMASK_ICON} alt="metamask" width={48} /><div className="w-full text-center">Metamask</div></div></div> :
+              <a href="https://metamask.io" className="" target="_blank" rel="noreferrer"><div className="flex items-center text-[24px] text-black/80 gap-4 border-2 p-4 px-8 cursor-pointer"><img src={METAMASK_ICON} alt="metamask" width={48} />Install Metamask</div></a>
             }
+            <div className="items-center justify-center text-[24px] text-black/80 gap-2 md:gap-16 border-2 p-4 px-8 cursor-pointer grid md:flex"><div className="grid items-center gap-4 text-center justify-items-center"><img src={TRUSTWALLET_ICON} alt="metamask" width={48} /><div className="w-full text-center">TrustWallet</div></div></div>
           </div>
         </div>
       </div>
